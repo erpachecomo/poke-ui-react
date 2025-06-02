000d775795afcd0ad1db7f565398b80aa78d09e1
erpachecomo <epachecomorelos@gmail.com>
2025-06-02 09:44:43 -0700

    fix: export structure

--- src/components/Stat/Stat.tsx
+++ src/components/Stat/Stat.tsx
@@ -1,17 +1,4 @@
-export type StatProps = {
-  label: string;
-  value: number | string;
-  maxValue?: number;
-  minValue?: number;
-  classNames?: {
-    container?: string;
-    labelsContainer?: string;
-    label?: string;
-    value?: string;
-    bar?: string;
-    barFill?: string;
-  };
-};
+import type { StatProps } from "./types";
 
 const Stat: React.FC<StatProps> = ({ label, value, maxValue = 100, minValue = 0, classNames}) => {
     
