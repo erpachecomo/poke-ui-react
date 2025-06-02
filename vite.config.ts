369a313871f7076d808e5090caf545074810861a
erpachecomo <epachecomorelos@gmail.com>
2025-06-02 00:33:29 -0700

    fix: adding files to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
+    outDir: "lib",
     lib: {
       entry: 'src/index.ts',
       name: "PokeUI",
